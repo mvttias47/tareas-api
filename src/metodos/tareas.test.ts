b33f3aed79dfad0d514e60273aa6ba338ef72f92
mvttias47 <matias.munoz_n@mail.udp.cl>
2025-10-17 20:41:36 -0300

    fix: ruta relativa en test y desactivar ESLint en build

--- src/metodos/tareas.test.ts
+++ src/metodos/tareas.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-import * as prismaModule from '@/lib/prisma'; 
+import * as prismaModule from '../lib/prisma'; 
 import { listarTareas, crearTarea } from './tareas';
 
 describe('metodos/tareas', () => {
