21814212b4c27ded071cafc1649a1d84f6c59454
mvttias47 <matias.munoz_n@mail.udp.cl>
2025-10-17 21:30:56 -0300

    chore: use default prisma client location; remove src/generated

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-// next.config.ts
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, 
-  },
-  typescript: {
-    ignoreBuildErrors: true,  
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
